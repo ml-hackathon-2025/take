767065c0d9838a9e3a68e9756cafca442ffd827f
Erno Mitrovic <ernomitrovic@zohomail.com>
2025-09-20 11:44:21 +0200

    Backend initial commit

---
+++ backend/src/main/java/de/metalevel/take/TakeApplication.java
@@ -0,0 +1,13 @@
+package de.metalevel.take;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/metalevel/take/TakeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.metalevel.take;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
